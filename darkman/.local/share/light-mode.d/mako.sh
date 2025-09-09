8e509ebc0a586fea18f624429c6ef2ca15414421
Ossian Winter <ossian@winter.vg>
2025-09-09 21:42:53 +0200

    darkman: Fix incorrect mako active theme symlink path

--- darkman/.local/share/light-mode.d/mako.sh
+++ darkman/.local/share/light-mode.d/mako.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-ln -sf "${HOME}/.config/mako/themes/light" "${HOME}/.config/mako/themes/theme"
+ln -sf "${HOME}/.config/mako/themes/light" "${HOME}/.config/mako/themes/darkman"
 makoctl reload
