4ea4eff9858880a925a76480e54a89b4600dadc9
Ossian Winter <ossian@winter.vg>
2026-02-14 22:50:53 +0100

    Initial commit

---
+++ bash/.bash_profile
@@ -0,0 +1,7 @@
+# .bash_profile
+
+export EDITOR=emacs
+export VISUAL=emacs
+
+# Get the aliases and functions
+[ -f $HOME/.bashrc ] && . $HOME/.bashrc
---
+++ stow.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+
+stow() {
+    package=$1
+    target=$2
+    command stow --no-folding --restow --target="${target}" "${package}"
+}
+
+stow X11        "${HOME}"
+stow bash       "${HOME}"
+stow dbus       "${HOME}"
+stow emacs      "${HOME}"
+stow fontconfig "${HOME}"
+stow git        "${HOME}"
+stow gtk-3.0    "${HOME}"
+stow gtk-4.0    "${HOME}"
+stow pipewire   "${HOME}"
+stow readline   "${HOME}"
+stow turnstile  "${HOME}"
