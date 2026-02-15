2ac183ac825aedd7753c37ea15ad7f9dced55d9f
Ossian Winter <ossian@winter.vg>
2026-02-15 11:44:14 +0100

    emacs: Install consult

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -19,6 +19,27 @@
 (set-language-environment "UTF-8")
 (setq default-input-method nil)    ; side-effect of `set-language-environment'
 
+;;; Keymaps:
+
+;;;; C-x:
+
+(keymap-set ctl-x-map          "M-:" #'consult-complex-command)     ; replaces `repeat-complex-command'
+(keymap-set ctl-x-map          "b"   #'consult-buffer)              ; replaces `switch-to-buffer'
+(keymap-set ctl-x-4-map        "b"   #'consult-buffer-other-window) ; replaces `switch-to-buffer-other-window'
+(keymap-set ctl-x-5-map        "b"   #'consult-buffer-other-frame)  ; replaces `switch-to-buffer-other-frame'
+(keymap-set tab-prefix-map     "b"   #'consult-buffer-other-frame)  ; replaces `switch-to-buffer-other-tab'
+(keymap-set bookmark-map       "b"   #'consult-bookmark)            ; replaces `bookmark-jump'
+(keymap-set project-prefix-map "b"   #'consult-project-buffer)      ; replaces `project-switch-to-buffer'
+
+;;;; M-g:
+
+(keymap-set goto-map "g"   #'consult-goto-line) ; replaces `goto-line'
+(keymap-set goto-map "M-g" #'consult-goto-line) ; replaces `goto-line'
+
+;;;; M-s:
+
+(keymap-set search-map "g" #'consult-grep)
+
 ;;; Completion:
 
 (setq read-extended-command-predicate #'command-completion-default-include-p)
@@ -27,6 +48,9 @@
 
 (setq enable-recursive-minibuffers t)
 
+(unless (package-installed-p 'consult)
+  (package-install 'consult))
+
 (unless (package-installed-p 'vertico)
   (package-install 'vertico))
 (vertico-mode +1)
@@ -34,6 +58,11 @@
 (unless (package-installed-p 'marginalia)
   (package-install 'marginalia))
 (marginalia-mode +1)
+
+;;; Code navigation:
+
+(setq xref-show-xrefs-function       #'consult-xref)
+(setq xref-show-definitions-function #'consult-xref)
 
 ;;; Version control:
 
