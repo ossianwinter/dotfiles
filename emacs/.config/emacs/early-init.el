8207805e4ae4e5414f1dc094315012b6efb0e7c8
Ossian Winter <ossian@winter.vg>
2026-02-15 10:14:56 +0100

    emacs: Disable tool-bar by default

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,4 +1,7 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (setq menu-bar-mode nil)
 
+(push '(tool-bar-lines . 0) default-frame-alist)
+(setq tool-bar-mode nil)
+
 (add-to-list 'default-frame-alist '(font . "Berkeley Mono-16"))
