64934cce7a735aa12aadf6696ffbf39c40999bb3
Ossian Winter <ossian@winter.vg>
2026-02-15 10:57:26 +0100

    emacs: Set user mail address

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,4 +1,5 @@
-(setq user-full-name "Ossian Winter")
+(setq user-full-name "Ossian Winter"
+      user-mail-address "ossian@winter.vg")
 
 (set-language-environment "UTF-8")
 (setq default-input-method nil)    ; side-effect of `set-language-environment'
