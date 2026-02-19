0e64d952b6c613001565a922496d81d49cc558fb
Ossian Winter <ossian@winter.vg>
2026-02-19 18:43:20 +0100

    emacs: Reorganize headers

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -11,53 +11,19 @@
 
 ;;; Code:
 
-(require 'package)
-
-;;; System:
-
 (unless (bound-and-true-p server-process) (server-start))
 
 (set-language-environment "UTF-8")
 (setopt default-input-method nil)  ; side-effect of `set-language-environment'
 
-(setopt make-backup-files nil)
-
-;;; User:
-
 (setopt user-full-name "Ossian Winter")
 (setopt user-mail-address "ossian@winter.vg")
 
-;;; Keymaps:
-
-;;;; C-x:
-
-(keymap-set ctl-x-map          "M-:" #'consult-complex-command)     ; replaces `repeat-complex-command'
-(keymap-set ctl-x-map          "b"   #'consult-buffer)              ; replaces `switch-to-buffer'
-(keymap-set ctl-x-map          "+"   #'zoom)                        ; replaces `balance-windows'
-(keymap-set ctl-x-4-map        "b"   #'consult-buffer-other-window) ; replaces `switch-to-buffer-other-window'
-(keymap-set ctl-x-5-map        "b"   #'consult-buffer-other-frame)  ; replaces `switch-to-buffer-other-frame'
-(keymap-set tab-prefix-map     "b"   #'consult-buffer-other-frame)  ; replaces `switch-to-buffer-other-tab'
-(keymap-set bookmark-map       "b"   #'consult-bookmark)            ; replaces `bookmark-jump'
-(keymap-set project-prefix-map "b"   #'consult-project-buffer)      ; replaces `project-switch-to-buffer'
-
-;;;; M-g:
-
-(keymap-set goto-map "g"   #'consult-goto-line) ; replaces `goto-line'
-(keymap-set goto-map "M-g" #'consult-goto-line) ; replaces `goto-line'
-
-;;;; M-s:
-
-(keymap-set search-map "g" #'consult-ripgrep)
-(keymap-set search-map "f" #'consult-fd)
-
-;;;; Global:
-
-(keymap-global-set "M-y" #'consult-yank-pop)    ; replaces `yank-pop'
-(keymap-global-set "C-." #'avy-goto-char-timer) ; read N chars and jump to the first one
-(keymap-global-set "C-;" #'embark-act)          ; prompt for action and perform it
-(keymap-global-set "C-:" #'embark-dwim)         ; perform default action on current target
+(setopt make-backup-files nil)
 
 ;;; Package management:
+
+(require 'package)
 
 (setopt package-archives
 	'(("gnu"    . "https://elpa.gnu.org/packages/")
@@ -69,75 +35,102 @@
 	  ("nongnu" . 80)
 	  ("melpa"  . 70)))
 
-;;; Theme:
+;;; Visual:
 
 (require-theme 'modus-themes)
 (modus-themes-load-theme 'modus-vivendi)
 
-;;; Buffer management:
-
-(setopt consult-buffer-list-function #'consult--frame-buffer-list)
-
-(unless (package-installed-p 'shackle) (package-install 'shackle))
-(shackle-mode +1)
-
-;;; Window management:
-
-(unless (package-installed-p 'zoom) (package-install 'zoom))
-(setopt zoom-size '(0.618 . 0.618))
-
 ;;; Completion:
-
-(setopt completion-styles '(orderless))
-(setopt completion-category-overrides '((file (styles partial-completion))))
 
 (setopt read-extended-command-predicate #'command-completion-default-include-p)
 
+;;;; Orderless:
+
 (unless (package-installed-p 'orderless) (package-install 'orderless))
+
+(setopt completion-styles '(orderless))
+(setopt completion-category-overrides '((file (styles partial-completion))))
 
 ;;; Minibuffer:
 
 (setopt enable-recursive-minibuffers t)
 
+;;;; Vertico:
+
 (unless (package-installed-p 'vertico) (package-install 'vertico))
+
 (vertico-mode +1)
 
+;;;; Marginalia:
+
 (unless (package-installed-p 'marginalia) (package-install 'marginalia))
+
 (marginalia-mode +1)
+
+;;;; Embark:
 
 (unless (package-installed-p 'embark) (package-install 'embark))
 (unless (package-installed-p 'embark-consult) (package-install 'embark-consult))
 
+(keymap-global-set "C-;" #'embark-act)  ; prompt for action and perform it
+(keymap-global-set "C-:" #'embark-dwim) ; perform default action on current target
+
 ;;; Navigation:
+
+;;;; Consult:
 
 (unless (package-installed-p 'consult) (package-install 'consult))
 
-;;;; File navigation:
+(setopt consult-buffer-list-function     #'consult--frame-buffer-list)
+(setopt consult-preview-excluded-buffers  '(major-mode . exwm-mode))
+(setopt xref-show-xrefs-function         #'consult-xref)
+(setopt xref-show-definitions-function   #'consult-xref)
 
-;;;; Text navigation:
+(keymap-global-set "M-y" #'consult-yank-pop) ; replaces `yank-pop'
+
+(keymap-set ctl-x-map          "M-:" #'consult-complex-command)     ; replaces `repeat-complex-command'
+(keymap-set ctl-x-map          "b"   #'consult-buffer)              ; replaces `switch-to-buffer'
+(keymap-set ctl-x-4-map        "b"   #'consult-buffer-other-window) ; replaces `switch-to-buffer-other-window'
+(keymap-set ctl-x-5-map        "b"   #'consult-buffer-other-frame)  ; replaces `switch-to-buffer-other-frame'
+(keymap-set tab-prefix-map     "b"   #'consult-buffer-other-frame)  ; replaces `switch-to-buffer-other-tab'
+(keymap-set bookmark-map       "b"   #'consult-bookmark)            ; replaces `bookmark-jump'
+(keymap-set project-prefix-map "b"   #'consult-project-buffer)      ; replaces `project-switch-to-buffer'
+
+(keymap-set goto-map "g"   #'consult-goto-line) ; replaces `goto-line'
+(keymap-set goto-map "M-g" #'consult-goto-line) ; replaces `goto-line'
+
+(keymap-set search-map "g" #'consult-ripgrep) ; search with rg
+(keymap-set search-map "f" #'consult-fd)      ; search with fd
+
+;;;; avy:
 
 (unless (package-installed-p 'avy) (package-install 'avy))
 
-;;;; Code navigation:
-
-(setq xref-show-xrefs-function       #'consult-xref)
-(setq xref-show-definitions-function #'consult-xref)
+(keymap-global-set "C-." #'avy-goto-char-timer) ; read N chars and jump to the first one
 
 ;;; Version control:
+
+;;;; Magit:
 
 (unless (package-installed-p 'magit) (package-install 'magit))
 
 ;;; Misc:
 
+;;;; Eat:
+
 (unless (package-installed-p 'eat) (package-install 'eat))
 
 ;;; X11:
 
-(unless (package-installed-p 'exwm) (package-install 'exwm))
-(require 'exwm)
+;;;; filechooser:
 
-(add-hook 'exwm-update-class-hook (lambda () (exwm-workspace-rename-buffer exwm-class-name)))
-(add-hook 'exwm-update-title-hook (lambda () (exwm-workspace-rename-buffer exwm-title)))
+(unless (package-installed-p 'filechooser) (package-install 'filechooser))
+
+(setopt filechooser-use-popup-frame nil)
+
+;;;; EXWM:
+
+(unless (package-installed-p 'exwm) (package-install 'exwm))
 
 (setopt exwm-input-global-keys
 	`(([?\s-r] . exwm-reset)
@@ -168,21 +161,11 @@
 	  ([?\C-y]   . [?\C-v])
 	  ([?\C-s]   . [?\C-f])))
 
-;; @Note: `exwm-wm-mode' is enabled later
-
-;; https://github.com/minad/consult/issues/178
-;; https://github.com/minad/consult/issues/186
-;; https://github.com/minad/consult/issues/204
-;; https://github.com/minad/consult/wiki#do-not-preview-exwm-windows-or-tramp-buffers
-(setopt consult-preview-excluded-buffers '(major-mode . exwm-mode))
-
-(unless (package-installed-p 'filechooser) (package-install 'filechooser))
-(setopt filechooser-use-popup-frame nil)
-
-;;; Postlude:
+(add-hook 'exwm-update-class-hook (lambda () (exwm-workspace-rename-buffer exwm-class-name)))
+(add-hook 'exwm-update-title-hook (lambda () (exwm-workspace-rename-buffer exwm-title)))
 
 (load custom-file 'noerror 'nomessage)
-(when (exwm--find-x-frame) (exwm-wm-mode +1)) ; let `custom-file' override settings defined in this file
+(exwm-wm-mode +1)
 
 (provide 'init)
 ;;; init.el ends here
