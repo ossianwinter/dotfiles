2c11ca4061fe7a7e045a594901967b74026a7db3
Ossian Winter <ossian@winter.vg>
2026-03-30 17:59:48 +0200

    modules/email: Remove systemd services

--- modules/email.nix
+++ modules/email.nix
@@ -6,34 +6,4 @@
   ];
 
   programs.msmtp.enable = true;
-
-  systemd.user = {
-    services.mbsync = {
-      description = "mbsync";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.isync}/bin/mbsync --all";
-      };
-      unitConfig = {
-        OnSuccess = [ "mu-index.service" ];
-      };
-    };
-
-    services.mu-index = {
-      description = "mu index";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.mu}/bin/mu index";
-      };
-    };
-
-    timers.mbsync = {
-      description = "mbsync";
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnCalendar = "*:0/5";
-        Unit = "mbsync.service";
-      };
-    };
-  };
 }
