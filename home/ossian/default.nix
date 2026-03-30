a6e98bacdaadb82b8e6b4fe4d299e2350dbe258d
Ossian Winter <ossian@winter.vg>
2026-03-30 23:27:00 +0200

    home/ossian/firefox: Configure Firefox

--- home/ossian/default.nix
+++ home/ossian/default.nix
@@ -5,19 +5,23 @@
     ./email.nix
   ];
 
-  programs.git = {
-    enable = true;
-    package = pkgs.gitFull;
-    signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBATgOCSG5++OrEi+r/gcwo7oSUGKCKxVdGV87povcfO";
-      format = "ssh";
-      signByDefault = true;
-      signer = "${pkgs._1password-gui}/bin/op-ssh-sign";
-    };
-    settings = {
-      user = {
-        name = "Ossian Winter";
-        email = "ossian@winter.vg";
+  programs = {
+    firefox.enable = true;
+
+    git = {
+      enable = true;
+      package = pkgs.gitFull;
+      signing = {
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBATgOCSG5++OrEi+r/gcwo7oSUGKCKxVdGV87povcfO";
+        format = "ssh";
+        signByDefault = true;
+        signer = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      };
+      settings = {
+        user = {
+          name = "Ossian Winter";
+          email = "ossian@winter.vg";
+        };
       };
     };
   };
