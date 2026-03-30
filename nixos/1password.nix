cf8e393b699c75ff716872d16792671483dbcb6f
Ossian Winter <ossian@winter.vg>
2026-03-30 22:19:42 +0200

    nixos/1password: Enable GUI by default

--- nixos/1password.nix
+++ nixos/1password.nix
@@ -1,14 +1,8 @@
 { config, lib, ... }:
 {
-  programs._1password.enable = true;
-  programs._1password-gui.enable = lib.mkIf config.services.graphical-desktop.enable (
-    lib.mkDefault true
-  );
+  programs._1password.enable = lib.mkDefault true;
+  programs._1password-gui.enable = lib.mkDefault true;
 
   # 1Password uses polkit for the (optional) system authentication service,
   security.polkit.enable = lib.mkIf config.programs._1password-gui.enable (lib.mkDefault true);
-
-  programs.git.config = lib.mkIf config.programs._1password-gui.enable {
-    gpg.ssh.program = lib.mkDefault "${config.programs._1password-gui.package}/bin/op-ssh-sign";
-  };
 }
