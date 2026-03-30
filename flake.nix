ed85ca0a19897d0317dcc95f6a4ed667f52026ab
Ossian Winter <ossian@winter.vg>
2026-03-30 21:24:40 +0200

    nixos: Reparent modules

--- flake.nix
+++ flake.nix
@@ -7,16 +7,16 @@
       nixosConfigurations.workstation = nixpkgs.lib.nixosSystem {
         modules = [
           ./machines/workstation
-          ./modules/1password.nix
-          ./modules/audio.nix
-          ./modules/darkman.nix
-          ./modules/email.nix
-          ./modules/firefox.nix
-          ./modules/fonts.nix
-          ./modules/git.nix
-          ./modules/gnome-keyring.nix
-          ./modules/steam.nix
-          ./modules/sway.nix
+          ./nixos/1password.nix
+          ./nixos/audio.nix
+          ./nixos/darkman.nix
+          ./nixos/email.nix
+          ./nixos/firefox.nix
+          ./nixos/fonts.nix
+          ./nixos/git.nix
+          ./nixos/gnome-keyring.nix
+          ./nixos/steam.nix
+          ./nixos/sway.nix
           ./users/ossian
           { users.users.ossian.extraGroups = [ "wheel" ]; }
         ];
