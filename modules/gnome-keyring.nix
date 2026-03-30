566f6bc8bff88b30ed5dd44a5b6b29962d858edd
Ossian Winter <ossian@winter.vg>
2026-03-30 17:37:10 +0200

    modules/gnome-keyring: Set secret portal only if service is enabled

--- modules/gnome-keyring.nix
+++ modules/gnome-keyring.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ config, lib, ... }:
 {
   services.gnome.gnome-keyring.enable = true;
+
   xdg.portal.config.common = {
-    "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+    "org.freedesktop.impl.portal.Secret" = lib.mkIf config.services.gnome.gnome-keyring.enable (
+      lib.mkDefault [ "gnome-keyring" ]
+    );
   };
 }
