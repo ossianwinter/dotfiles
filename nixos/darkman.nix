9e5d884556b8177847fa6509a82464f071101741
Ossian Winter <ossian@winter.vg>
2026-03-30 22:24:59 +0200

    nixos/darkman: Remove module

--- nixos/darkman.nix
+++
@@ -1,9 +0,0 @@
-{ lib, pkgs, ... }:
-{
-  environment.systemPackages = with pkgs; [ darkman ];
-
-  xdg.portal.enable = lib.mkDefault true;
-  xdg.portal.config.common = {
-    "org.freedesktop.impl.portal.Settings" = lib.mkDefault [ "darkman" ];
-  };
-}
