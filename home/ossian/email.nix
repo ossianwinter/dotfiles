9a3a3899f4f19ff4e327364b7ec54d0f298530e0
Ossian Winter <ossian@winter.vg>
2026-04-01 21:19:40 +0200

    home/ossian/email: Remove mbsync

--- home/ossian/email.nix
+++ home/ossian/email.nix
@@ -7,16 +7,5 @@
     realName = "Ossian Winter";
     userName = "ossian@fastmail.com";
     passwordCommand = "op read op://iadi64yxnoyh2b5xosdxiydcna/i7o5bqrpenxguh5psiaave5k7m/credential";
-
-    mbsync = {
-      enable = true;
-      create = "both";
-      remove = "both";
-      expunge = "both";
-    };
-  };
-
-  programs = {
-    mbsync.enable = true;
   };
 }
