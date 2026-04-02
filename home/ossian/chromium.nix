259d16d91e1546818693e5e9362e17f45dfa1ca2
Ossian Winter <ossian@winter.vg>
2026-04-02 16:47:15 +0200

    home/ossian/chromium: Show only search completions

--- home/ossian/chromium.nix
+++ home/ossian/chromium.nix
@@ -24,6 +24,9 @@ in
   programs.chromium = {
     enable = true;
     package = pkgs.ungoogled-chromium;
+    commandLineArgs = [
+      "--omnibox-autocomplete-filtering=search"
+    ];
     extensions = map mkExtension extensions;
   };
 
