e1a06f641a5a8629ebb9ddcdd473d842abbe7617
Ossian Winter <ossian@winter.vg>
2026-04-25 17:13:44 +0200

    home/ossian/podman: Remove unused testcontainers properties

--- home/ossian/podman.nix
+++ home/ossian/podman.nix
@@ -4,8 +4,4 @@
     source = ./podman/containers;
     recursive = true;
   };
-
-  home.file.".testcontainers.properties".text = ''
-    docker.host=unix:///run/user/$XDG_RUNTIME_DIR/podman/podman.sock
-  '';
 }
