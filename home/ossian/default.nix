a4d885a72d6ed05c1af1c18deeedcea4db36e6ab
Ossian Winter <ossian@winter.vg>
2026-05-03 12:39:53 +0200

    home/ossian: Add Discord krisp-patcher

--- home/ossian/default.nix
+++ home/ossian/default.nix
@@ -70,7 +70,31 @@
   };
 
   home = {
-    packages = with pkgs; [ xdg-utils jetbrains.idea oama ];
+    packages =
+      let
+        krisp-patcher =
+          pkgs.writers.writePython3Bin "krisp-patcher"
+            {
+              libraries = with pkgs.python3Packages; [
+                capstone
+                pyelftools
+              ];
+              flakeIgnore = [
+                "E501" # line too long (82 > 79 characters)
+                "F403" # 'from module import *' used; unable to detect undefined names
+                "F405" # name may be undefined, or defined from star imports: module
+              ];
+            }
+            (
+              builtins.readFile (
+                pkgs.fetchurl {
+                  url = "https://pastebin.com/raw/8tQDsMVd";
+                  sha256 = "sha256-IdXv0MfRG1/1pAAwHLS2+1NESFEz2uXrbSdvU9OvdJ8=";
+                }
+              )
+            );
+      in
+        with pkgs; [ xdg-utils jetbrains.idea oama krisp-patcher ];
 
     stateVersion = "25.11";
   };
