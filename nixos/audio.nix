bdaa2ed995c6b89915e5d53e21db82b6e918cc11
Ossian Winter <ossian@winter.vg>
2026-05-18 19:21:33 +0200

    nixos/audio: Enable JACK audio emulation

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -2,6 +2,7 @@
 {
   services.pipewire.enable = true;
   services.pipewire.pulse.enable = true;
+  services.pipewire.jack.enable = true;
 
   # Used by PipeWire to acquire realtime scheduling priority.
   security.rtkit.enable = true;
