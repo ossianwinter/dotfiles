cff07e458892c5f936580b83e7bad981a933f1a8
Ossian Winter <ossian@winter.vg>
2026-05-18 19:32:41 +0200

    nixos/audio: nixos/audio: Pull pw-jack into the global environment

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.pipewire.enable = true;
   services.pipewire.pulse.enable = true;
@@ -6,4 +6,7 @@
 
   # Used by PipeWire to acquire realtime scheduling priority.
   security.rtkit.enable = true;
+
+  # Pulls pw-jack into the global environment.
+  environment.systemPackages = with pkgs; [ pipewire.jack ];
 }
